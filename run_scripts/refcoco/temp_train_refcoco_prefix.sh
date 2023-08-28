729a508540f3b0ba763686ead659bbdea7367040
jskim706 <jskim@islab.snu.ac.kr>
2023-08-28 15:12:39 +0900

    temp

--- run_scripts/refcoco/temp_train_refcoco_prefix.sh
+++ run_scripts/refcoco/temp_train_refcoco_prefix.sh
@@ -53,59 +53,59 @@ for max_epoch in {100,}; do
       CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=${MASTER_PORT} ../../train.py \
           $data \
           --selected-cols=${selected_cols} \
-#          --bpe-dir=${bpe_dir} \
-#          --user-dir=${user_dir} \
-#          --restore-file=${restore_file} \
-#          --reset-optimizer --reset-dataloader --reset-meters \
-#          --save-dir=${save_path} \
-#          --task=${task} \
-#          --arch=${arch} \
-#          --criterion=${criterion} \
-#          --label-smoothing=${label_smoothing} \
-#          --batch-size=${batch_size} \
-#          --update-freq=${update_freq} \
-#          --encoder-normalize-before \
-#          --decoder-normalize-before \
-#          --share-decoder-input-output-embed \
-#          --share-all-embeddings \
-#          --layernorm-embedding \
-#          --patch-layernorm-embedding \
-#          --code-layernorm-embedding \
-#          --resnet-drop-path-rate=${resnet_drop_path_rate} \
-#          --encoder-drop-path-rate=${encoder_drop_path_rate} \
-#          --decoder-drop-path-rate=${decoder_drop_path_rate} \
-#          --dropout=${dropout} \
-#          --attention-dropout=${attention_dropout} \
-#          --weight-decay=0.01 --optimizer=adam --adam-betas="(0.9,0.999)" --adam-eps=1e-08 --clip-norm=1.0 \
-#          --lr-scheduler=polynomial_decay --lr=${lr} \
-#          --max-epoch=${max_epoch} --warmup-ratio=${warmup_ratio} \
-#          --log-format=simple --log-interval=10 \
-#          --fixed-validation-seed=7 \
-#          --no-epoch-checkpoints --keep-best-checkpoints=1 \
-#          --save-interval=1 --validate-interval=1 \
-#          --save-interval-updates=500 --validate-interval-updates=500 \
-#          --eval-acc \
-#          --eval-args='{"beam":5,"min_len":4,"max_len_a":0,"max_len_b":4}' \
-#          --best-checkpoint-metric=score --maximize-best-checkpoint-metric \
-#          --max-src-length=${max_src_length} \
-#          --max-tgt-length=${max_tgt_length} \
-#          --find-unused-parameters \
-#          --add-type-embedding \
-#          --scale-attn \
-#          --scale-fc \
-#          --encoder-prompt \
-#          --decoder-prompt \
-#          --encoder-prompt-type=${prompt_type_method} \
-#          --decoder-prompt-type=${prompt_type_method} \
-#          --encoder-prompt-length=${encoder_prompt_length} \
-#          --decoder-prompt-length=${decoder_prompt_length} \
-#          --scale-heads \
-#          --disable-entangle \
-#          --num-bins=${num_bins} \
-#          --patch-image-size=${patch_image_size} \
-#          --fp16 \
-#          --fp16-scale-window=512 \
-#          --num-workers=0
+          --bpe-dir=${bpe_dir} \
+          --user-dir=${user_dir} \
+          --restore-file=${restore_file} \
+          --reset-optimizer --reset-dataloader --reset-meters \
+          --save-dir=${save_path} \
+          --task=${task} \
+          --arch=${arch} \
+          --criterion=${criterion} \
+          --label-smoothing=${label_smoothing} \
+          --batch-size=${batch_size} \
+          --update-freq=${update_freq} \
+          --encoder-normalize-before \
+          --decoder-normalize-before \
+          --share-decoder-input-output-embed \
+          --share-all-embeddings \
+          --layernorm-embedding \
+          --patch-layernorm-embedding \
+          --code-layernorm-embedding \
+          --resnet-drop-path-rate=${resnet_drop_path_rate} \
+          --encoder-drop-path-rate=${encoder_drop_path_rate} \
+          --decoder-drop-path-rate=${decoder_drop_path_rate} \
+          --dropout=${dropout} \
+          --attention-dropout=${attention_dropout} \
+          --weight-decay=0.01 --optimizer=adam --adam-betas="(0.9,0.999)" --adam-eps=1e-08 --clip-norm=1.0 \
+          --lr-scheduler=polynomial_decay --lr=${lr} \
+          --max-epoch=${max_epoch} --warmup-ratio=${warmup_ratio} \
+          --log-format=simple --log-interval=10 \
+          --fixed-validation-seed=7 \
+          --no-epoch-checkpoints --keep-best-checkpoints=1 \
+          --save-interval=1 --validate-interval=1 \
+          --save-interval-updates=500 --validate-interval-updates=500 \
+          --eval-acc \
+          --eval-args='{"beam":5,"min_len":4,"max_len_a":0,"max_len_b":4}' \
+          --best-checkpoint-metric=score --maximize-best-checkpoint-metric \
+          --max-src-length=${max_src_length} \
+          --max-tgt-length=${max_tgt_length} \
+          --find-unused-parameters \
+          --add-type-embedding \
+          --scale-attn \
+          --scale-fc \
+          --encoder-prompt \
+          --decoder-prompt \
+          --encoder-prompt-type=${prompt_type_method} \
+          --decoder-prompt-type=${prompt_type_method} \
+          --encoder-prompt-length=${encoder_prompt_length} \
+          --decoder-prompt-length=${decoder_prompt_length} \
+          --scale-heads \
+          --disable-entangle \
+          --num-bins=${num_bins} \
+          --patch-image-size=${patch_image_size} \
+          --fp16 \
+          --fp16-scale-window=512 \
+          --num-workers=0
 #          --num-workers=0 > ${log_file} 2>&1
     done
   done
