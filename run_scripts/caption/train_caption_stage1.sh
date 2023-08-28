16b05f7276815e74e8ad0a127140f35054000c72
jskim706 <jskim@islab.snu.ac.kr>
2023-08-28 18:54:59 +0900

    temp

--- run_scripts/caption/train_caption_stage1.sh
+++ run_scripts/caption/train_caption_stage1.sh
@@ -54,7 +54,7 @@ for max_epoch in 2; do
       save_path=${save_dir}/${max_epoch}"_"${warmup_ratio}"_"${drop_worst_after}
       mkdir -p $save_path
 
-      CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=${MASTER_PORT} ../../train.py \
+      CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=${MASTER_PORT} train.py \
           $data \
           --selected-cols=${selected_cols} \
           --bpe-dir=${bpe_dir} \
