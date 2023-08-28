b4db61396cb434c685bcd6893004d8145f6527c5
jskim706 <jskim@islab.snu.ac.kr>
2023-08-28 15:08:43 +0900

    temp

--- run_scripts/refcoco/temp_train_refcoco_prefix.sh
+++ run_scripts/refcoco/temp_train_refcoco_prefix.sh
@@ -105,7 +105,7 @@ for max_epoch in {100,}; do
 #          --patch-image-size=${patch_image_size} \
 #          --fp16 \
 #          --fp16-scale-window=512 \
-          --num-workers=0
+#          --num-workers=0
 #          --num-workers=0 > ${log_file} 2>&1
     done
   done
