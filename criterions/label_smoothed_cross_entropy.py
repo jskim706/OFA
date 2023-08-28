7471ea5b90c5771d0952f116eb594c5bf4778bf9
jskim706 <jskim@islab.snu.ac.kr>
2023-08-28 19:59:32 +0900

    temp

--- criterions/label_smoothed_cross_entropy.py
+++ criterions/label_smoothed_cross_entropy.py
@@ -220,8 +220,11 @@ class AdjustLabelSmoothedCrossEntropyCriterion(FairseqCriterion):
         if "constraint_masks" in sample and sample["constraint_masks"] is not None:
             constraint_masks = sample["constraint_masks"]
             print(f"net_output[0] : {net_output[0]}")
+            print(f"net_output[0] : {net_output[0].shape}")
             print(f"constraint_masks : {constraint_masks}")
+            print(f"constraint_masks : {constraint_masks.shape}")
             print(f"~constraint_masks : {~constraint_masks}")
+            print(f"~constraint_masks : {~constraint_masks.shape}")
             print(f"-math.inf : {-math.inf}")
             net_output[0] = net_output[0].masked_fill(~constraint_masks, -math.inf)
         if self.constraint_start is not None and self.constraint_end is not None:
