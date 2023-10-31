1e13566161aa226ec44304af2780e6a244335b91
jskim706 <jskim@islab.snu.ac.kr>
2023-10-31 19:23:53 +0900

    unify_multihead_attention.py key_padding_mask edit

--- models/ofa/unify_multihead_attention.py
+++ models/ofa/unify_multihead_attention.py
@@ -339,7 +339,13 @@ class MultiheadAttention(nn.Module):
             v = torch.cat([prompt_v, v], dim=1)
         if key_padding_mask is not None:
             assert key_padding_mask.size(0) == bsz
-            assert key_padding_mask.size(1) == k.size(1), "AssertionError key_padding_mask.size(1) = {}, k.size(1) = {}".format(key_padding_mask.size(1), k.size(1))
+            if key_padding_mask.size(1) < k.size(1):
+                key_padding_mask = torch.cat(
+                    [key_padding_mask, k[:,key_padding_mask.size(1)-k.size(1):]], dim=1)
+            else:
+                assert key_padding_mask.size(1) == k.size(1), "AssertionError key_padding_mask.size(1) = {}, k.size(1) = {}".format(key_padding_mask.size(1), k.size(1))
+
+
         attn_weights = torch.bmm(q, k.transpose(1, 2))
         attn_weights = self.apply_sparse_mask(attn_weights, tgt_len, k.size(1), bsz)
 
