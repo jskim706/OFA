f2c853a407d07c3b2ff0a122b54367c16bb14cb1
jskim706 <jskim@islab.snu.ac.kr>
2023-11-17 22:44:19 +0900

    update

--- run_scripts/vqa/evaluate_vqa_beam_base_dh_bestrun_7.sh
+++ run_scripts/vqa/evaluate_vqa_beam_base_dh_bestrun_7.sh
@@ -19,22 +19,25 @@ path=/home/jskim/Projects/OFA/vqa_checkpoints_lora_1116/10_0.04_5e-5_480_stage1/
 
 result_path=results/vqa_${split}_beam_vqa_checkpoints_lora_111610_0.04_5e-5_480_stage1
 selected_cols=0,5,2,3,4
-valid_batch_size=20
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=${MASTER_PORT} evaluate.py \
     ${data} \
     --path=${path} \
     --user-dir=${user_dir} \
     --task=vqa_gen \
-    --batch-size=16 \
+    --selected-cols=${selected_cols} \
+    --bpe-dir=${bpe_dir} \
+    --patch-image-size=480 \
+    --prompt-type='none' \
+    --batch-size=8 \
     --log-format=simple --log-interval=10 \
     --seed=7 \
     --gen-subset=${split} \
     --results-path=${result_path} \
     --fp16 \
-    --ema-eval \
     --beam-search-vqa-eval \
-    --beam=5 \
+    --zero-shot \
+    --beam=20 \
     --unnormalized \
     --temperature=1.0 \
-    --num-workers=0 \
+    --num-workers=0
